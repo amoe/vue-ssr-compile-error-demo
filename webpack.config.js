7d26e5929c65cf13b81ee2a9bcd7dd6caf1731ce
David Banks <amoebae@gmail.com>
2017-04-28 13:22:39 +0100

    adding bug repro

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
+
 module.exports = {
+    target: 'node',
     output: {
-        filename: "bundle.js"
+        filename: "bundle.js",
+        libraryTarget: 'commonjs2'
     },
-    entry: "./main.js"
+    entry: "./nonexistent.js",
+    plugins: [
+        new VueSSRServerPlugin()
+    ]
 };
